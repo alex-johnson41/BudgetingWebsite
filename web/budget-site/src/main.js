b35e7e1e088442312a991a9b93a6748ff3029622
alex-johnson41 <alex@heathjohnson.org>
2024-06-23 18:51:35 -0500

    Working front-end

--- web/budget-site/src/main.js
+++ web/budget-site/src/main.js
@@ -3,11 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import vuetify from 'vuetify'
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+
+const vuetify = createVuetify()
 
 const app = createApp(App)
 
 app.use(router)
-app.user(vuetify)
+app.use(vuetify)
 
 app.mount('#app')
