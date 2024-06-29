0a26901457462f9839cb21eac06659bd42715f8f
alex-johnson41 <alex@heathjohnson.org>
2024-06-28 22:46:10 -0500

    Working vuetify/vite

--- web/budget-site/src/main.js
+++ web/budget-site/src/main.js
@@ -3,10 +3,17 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { createVuetify } from 'vuetify'
+// Vuetify
 import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'material-design-icons/iconfont/material-icons.css'
 
-const vuetify = createVuetify()
+const vuetify = createVuetify({
+    components,
+    directives
+})
 
 const app = createApp(App)
 
